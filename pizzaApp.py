5c9be004247f01fff7265e0fc42e5527d612bfc9
JakubWroblewski85 <jakub.wroblewski.alk@gmail.com>
2021-11-19 15:09:03 +0100

    fix pizza.py

--- pizzaApp.py
+++ pizzaApp.py
@@ -1,4 +1,4 @@
-# changed to rectagular pizza2.
+# changed to rectagular pizza
 
 pizza1_price = float(input('Cena pizzy 1: '))
 pizza1_a = float(input('Bok krótszy pizza1:'))
@@ -7,11 +7,11 @@ pizza2_price = float(input('Cena pizzy 2: '))
 pizza2_a = float(input('Bok krótszy pizza2:'))
 pizza2_b = float(input('Bok dłuższy pizza2:'))
 
-pole1 = pizza1_a * pizza2_b * 2
+pole1 = pizza1_a * pizza2_b
 pole2 = pizza2_a * pizza2_b
 
 a = pizza1_price/pole1
-b = pizza2_price/pole2*pole2
+b = pizza2_price/pole2
 
 if a < b:
     print('Pizza 1 opłaca się bardziej')
